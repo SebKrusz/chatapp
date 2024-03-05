8e96f81c935f222fa1e85e840b60ade097e2bbf5
SebKrusz <145293986+SebKrusz@users.noreply.github.com>
2024-03-04 23:27:24 -0800

    login page displaying

--- app/(auth)/page.jsx
+++ app/(auth)/page.jsx
@@ -1,9 +1,7 @@
-import React from "react";
 import Form from "@components/Form";
 
 const Login = () => {
-	return;
-	<Form type="login" />;
+	return <Form type="login" />;
 };
 
 export default Login;
