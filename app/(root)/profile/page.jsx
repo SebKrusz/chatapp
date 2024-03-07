5a5687761f1564ed85ed987574cc3525619d03c0
SebKrusz <145293986+SebKrusz@users.noreply.github.com>
2024-03-07 00:09:42 -0800

    cloudinary images uploading to mongodb

--- app/(root)/profile/page.jsx
+++ app/(root)/profile/page.jsx
@@ -1,48 +1,103 @@
 "use client";
 
+import Loader from "@components/Loader";
 import { PersonOutline } from "@mui/icons-material";
 import { useSession } from "next-auth/react";
-import React from "react";
+import { CldUploadButton } from "next-cloudinary";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
 const Profile = () => {
 	const { data: session } = useSession();
 	const user = session?.user;
+
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		if (user) {
+			reset({
+				username: user?.username,
+				profileImage: user?.profileImage,
+			});
+		}
+		setLoading(false);
+	}, [user]);
+
 	const {
 		register,
 		watch,
 		setValue,
+		reset,
 		handleSubmit,
-		formState: { errors },
+		formState: { error },
 	} = useForm();
-	return (
+
+	const uploadPhoto = (result) => {
+		setValue("profileImage", result?.info?.secure_url);
+	};
+	const updateUser = async (data) => {
+		setLoading(true);
+		try {
+			const res = await fetch(`/api/users/${user._id}/update`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(data),
+			});
+
+			setLoading(false);
+			window.location.reload();
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return loading ? (
+		<Loader />
+	) : (
 		<div className="profile-page">
 			<h1 className="text-heading3-bold">Edit Your Profile</h1>
-			<form className="edit-profile">
+
+			<form className="edit-profile" onSubmit={handleSubmit(updateUser)}>
 				<div className="input">
 					<input
 						{...register("username", {
 							required: "Username is required",
 							validate: (value) => {
 								if (value.length < 3) {
-									return "Username must be at least 3 characters long";
+									return "Username must be at least 3 characters";
 								}
 							},
 						})}
 						type="text"
-						placeholder="username"
+						placeholder="Username"
 						className="input-field"
 					/>
 					<PersonOutline sx={{ color: "#737373" }} />
 				</div>
+				{error?.username && (
+					<p className="text-red-500">{error.username.message}</p>
+				)}
+
 				<div className="flex items-center justify-between">
 					<img
-						src={user?.profileImage || "/assets/person.jpg"}
+						src={
+							watch("profileImage") ||
+							user?.profileImage ||
+							"/assets/person.jpg"
+						}
 						alt="profile"
 						className="w-40 h-40 rounded-full"
 					/>
-					<p className="text-body-bold">Upload new photo</p>
+					<CldUploadButton
+						options={{ maxFiles: 1 }}
+						onSuccess={uploadPhoto}
+						uploadPreset="l66pngm1">
+						<p className="text-body-bold">Upload new photo</p>
+					</CldUploadButton>
 				</div>
+
 				<button className="btn" type="submit">
 					Save Changes
 				</button>
