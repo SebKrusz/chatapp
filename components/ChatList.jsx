5e4e5f5325454eb0fa4302556f94f61edec727e9
SebKrusz <145293986+SebKrusz@users.noreply.github.com>
2024-03-14 23:28:02 -0700

    api call for chat list component working with loader functional

--- components/ChatList.jsx
+++ components/ChatList.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { use, useState } from "react";
+import { useSession } from "next-auth/react";
 
 const ChatList = () => {
-	return (
+	const { data: sessions } = useSession();
+	const currentUser = sessions?.user;
+	const [loading, setLoading] = useState(true);
+	const [chats, setChats] = useState([]);
+
+	const getChats = async () => {
+		try {
+			const res = await fetch(`/api/users/${currentUser._id}`);
+			const data = await res.json();
+			setChats(data);
+			setLoading(false);
+		} catch (err) {
+			console.log(err);
+			return;
+		}
+	};
+
+	useEffect(() => {
+		if (currentUser) {
+			getChats();
+		}
+	}, [currentUser]);
+	return loading ? (
+		<Loader />
+	) : (
 		<div className="chat-list">
 			<input placeholder="Search chat..." className="input-search" />
 		</div>
