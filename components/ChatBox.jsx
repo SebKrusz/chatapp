730e4a72e812818b257e93dac7987be63a0d98b2
SebKrusz <145293986+SebKrusz@users.noreply.github.com>
2024-03-14 23:57:35 -0700

    group photos displaying with correct timestamp

--- components/ChatBox.jsx
+++ components/ChatBox.jsx
@@ -1,7 +1,52 @@
+import { format } from "date-fns";
 import React from "react";
 
-const ChatBox = () => {
-	return <div>ChatBox</div>;
+const ChatBox = ({ chat, currentUser }) => {
+	const otherMembers = chat?.members?.filter(
+		(member) => member !== currentUser._id
+	);
+
+	const lastMessage =
+		chat?.messages?.length > 0 &&
+		chat?.messages[chat?.messages.length - 1]?.text;
+	return (
+		<div className="chat-box">
+			<div className="class-info">
+				{chat?.isGroup ? (
+					<img
+						src={chat?.groupPhoto || "/assets/group.png"}
+						alt="group-photo"
+						className="profilePhoto"
+					/>
+				) : (
+					<img
+						src={
+							otherMembers[0].profilePhoto || "/assets/person.jpg"
+						}
+						alt="profile-photo"
+						className="profilePhoto"
+					/>
+				)}
+				<div className="flex flex-col gap-1">
+					{chat?.isGroup ? (
+						<p className="text-base-bold">{chat?.name}</p>
+					) : (
+						<p className="text-base-bold">
+							{otherMembers[0]?.username}
+						</p>
+					)}
+					{!lastMessage && (
+						<p className="text-small-bold"> Started a chat</p>
+					)}
+				</div>
+			</div>
+			<div>
+				<p className="text-base-light text-grey-3 ">
+					{!lastMessage && format(new Date(chat?.createdAt), "p")}
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default ChatBox;
